dd9159148dbd443142d6831f64cb9d73d59bc776
Franco Cortesi <cortesifranco3@gmail.com>
2022-10-08 15:28:10 -0300

    fix start node scripts

--- start_node_mainnet.sh
+++ start_node_mainnet.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # config:  https://hydra.iohk.io/build/13695229/download/1/index.html
 # config as pointed in cardano-node repo: https://book.world.dev.cardano.org/environments.html
-./cardano-node run \
-  --config ./cardano-node-mainnet/mainnet-config.json \
-  --database-path ./cardano-node-mainnet/db/ \
-  --socket-path ./cardano-node-mainnet/node.socket \
+./bin/cardano-node run \
+  --config ./data/cardano-node-mainnet/mainnet-config.json \
+  --database-path ./data/cardano-node-mainnet/db/ \
+  --socket-path ./data/cardano-node-mainnet/node.socket \
   --port 3001 \
-  --topology ./cardano-node-mainnet/mainnet-topology.json
+  --topology ./data/cardano-node-mainnet/mainnet-topology.json
--- start_node_testnet.sh
+++ start_node_testnet.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # config:  https://hydra.iohk.io/build/13695229/download/1/index.html
 # config as pointed in cardano-node repo: https://book.world.dev.cardano.org/environments.html
-./cardano-node run \
-  --config ./cardano-node-testnet/config.json \
-  --database-path ./cardano-node-testnet/db/ \
-  --socket-path ./cardano-node-testnet/node.socket \
+./bin/cardano-node run \
+  --config ./data/cardano-node-testnet/config.json \
+  --database-path ./data/cardano-node-testnet/db/ \
+  --socket-path ./data/cardano-node-testnet/node.socket \
   --port 3001 \
-  --topology ./cardano-node-testnet/topology.json
+  --topology ./data/cardano-node-testnet/topology.json
