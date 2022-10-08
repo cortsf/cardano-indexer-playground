db366aa85028a4c7b2f2e30cb66390ab7a6cd7e7
Franco Cortesi <cortesifranco3@gmail.com>
2022-10-08 15:13:54 -0300

    fix typos

--- start-node-mainnet.sh
+++ start-node-mainnet.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # config:  https://hydra.iohk.io/build/13695229/download/1/index.html
-# onfig as pointer in cardano-node repo: https://book.world.dev.cardano.org/environments.html
+# config as pointed in cardano-node repo: https://book.world.dev.cardano.org/environments.html
 ./cardano-node run \
   --config ./cardano-node-mainnet/mainnet-config.json \
   --database-path ./cardano-node-mainnet/db/ \
--- start-node-testnet.sh
+++ start-node-testnet.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # config:  https://hydra.iohk.io/build/13695229/download/1/index.html
-# config as pointer in cardano-node repo: https://book.world.dev.cardano.org/environments.html
+# config as pointed in cardano-node repo: https://book.world.dev.cardano.org/environments.html
 ./cardano-node run \
   --config ./cardano-node-testnet/config.json \
   --database-path ./cardano-node-testnet/db/ \
