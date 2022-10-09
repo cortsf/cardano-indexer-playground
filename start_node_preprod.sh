0ea10dba4f1cf2a5262b1739754812914ffc0bc1
Franco Cortesi <cortesifranco3@gmail.com>
2022-10-09 17:53:46 -0300

    preprod node port

--- start_node_preprod.sh
+++ start_node_preprod.sh
@@ -4,4 +4,4 @@
   --topology ./config/cardano-node-preprod/topology.json \
   --database-path ./data/cardano-node-preprod/db/ \
   --socket-path ./data/cardano-node-preprod/node.socket \
-  --port 3002
+  --port 3003
