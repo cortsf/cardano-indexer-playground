889e5609f3d7a5a7b1a585b8deefe1e8c6e2960a
Franco Cortesi <cortesifranco3@gmail.com>
2022-10-09 12:28:54 -0300

    add scrolls starters

---
+++ start_scrolls.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+RUST_LOG=info ./bin/scrolls daemon --config ./config/scrolls.toml
+
---
+++ start_scrolls_mainnet.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bas
+
+RUST_LOG=info ./bin/scrolls daemon --config ./config/scrolls_mainnet.toml
