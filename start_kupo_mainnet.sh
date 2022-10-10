9d37d9251ee223cb1107076de2333db3d7b0153c
Franco Cortesi <cortesifranco3@gmail.com>
2022-10-10 20:07:05 -0300

    fix kupo mainnet

--- start_kupo_mainnet.sh
+++ start_kupo_mainnet.sh
@@ -4,5 +4,5 @@
   --node-socket ./data/cardano-node-mainnet/node.socket \
   --node-config ./config/cardano-node-mainnet/mainnet-config.json \
   --workdir ./data/kupo-mainnet-db \
-  --since "$last_alonzo_block" \
+  --since "39916796.e72579ff89dc9ed325b723a33624b596c08141c7bd573ecfff56a1f7229e4d09" \
   --match "stake1uy6f5wg695g33ck8emlyx97vyfxyw8pmvgu5aq6hxgxscgg9s58ah"
