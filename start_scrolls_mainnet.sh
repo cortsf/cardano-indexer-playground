65a60b59c3e91cb18ea14c4d42e8568b6de59914
Franco Cortesi <cortesifranco3@gmail.com>
2022-10-11 15:10:01 -0300

    scrolls testnet config

--- start_scrolls_mainnet.sh
+++ start_scrolls_mainnet.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 RUST_LOG=info ./bin/scrolls daemon --console plain --config ./config/scrolls_mainnet.toml
+# RUST_LOG=info ./bin/scrolls daemon --console plain --config ./config/scrolls_mainnet_RANDOM.toml # WORKING DEMO USING RECENT DATA.
