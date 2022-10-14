9cd2517e0eba10b0c17383de0f03cf7730a459e5
Franco Cortesi <cortesifranco3@gmail.com>
2022-10-14 09:39:48 -0300

    add missing files

---
+++ start_scrolls_preprod.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+RUST_LOG=info ./bin/scrolls daemon --console plain --config ./config/scrolls_preprod.toml
+
---
+++ start_scrolls_testnet.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+RUST_LOG=info ./bin/scrolls daemon --console plain --config ./config/scrolls_testnet.toml
