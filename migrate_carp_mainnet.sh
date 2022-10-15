07c8f3c51ed8ac8da1532c9bd870ff83e19763df
Franco Cortesi <cortesifranco3@gmail.com>
2022-10-15 12:56:09 -0300

    carp migrate

---
+++ migrate_carp_mainnet.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cd ./repos/carp/indexer && cargo migrate up
--- start_carp_mainnet.sh
+++ start_carp_mainnet.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Notes
+########################################
+# Run migrate_carp.sh first to create db schema!
+
 export NETWORK=mainnet
 export SOCKET="./data/cardano-node-mainnet/node.socket"
 export POSTGRES_HOST=localhost
@@ -10,5 +14,6 @@ export PGPASSFILE="$(realpath carp/secrets/.pgpass)"
 export DATABASE_URL=postgresql://${PGUSER}:${PGPASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}
 export PGURI=$DATABASE_URL
 
-./bin/carp --plan ./carp/indexer/execution_plans/default.toml
+./bin/carp --plan ./repos/carp/indexer/execution_plans/default.toml
+# ./bin/carp --start-block "18750593c67f637725a90d2161fad09f1093c6d048c43492d0f9603797ea55aa" --plan ./repos/carp/indexer/execution_plans/default.toml
 
